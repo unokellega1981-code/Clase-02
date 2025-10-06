8b5d33ca40862b61fb0584ca6b5f393d32fcfe1f
JACG <jacarreragonzalez@gmail.com>
2025-10-06 18:45:14 +0200

    Initial commit

---
+++ Ejercicio1/src/main/java/com/jacg/Ejercicio1/HolaMundoController.java
@@ -0,0 +1,24 @@
+package com.jacg.Ejercicio1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String holaMundo() {
+        return "Hola mundo";
+    }
+
+    @GetMapping("/hola/{name}")
+    public String holaNombre(@PathVariable String name) {
+        return "Hola " + name;
+    }
+
+    @GetMapping("/Cuadrado/{number}")
+    public String mumumu(@PathVariable int number){
+        return "El cuadrado de ese número es " + (int)Math.pow(number,2);
+    }
+}
